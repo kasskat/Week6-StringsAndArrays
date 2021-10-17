e66a8e0a3809d10e0a229c0ddb85156260fd0fec
Jekaterina <jekaterina.kassapova@gmail.com>
2021-10-17 21:42:40 +0300

    week6

---
+++ ACharacterCounter/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ACharacterCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutajal sisestada ees- ja perekonnanime
+            //programm kuvab, mitu 'a' tähte on kasutaja ees- ja perekonnanimes kokku
+
+            Console.WriteLine("Sisesta oma eesnimi: ");
+            string firstName = Console.ReadLine();
+
+            Console.WriteLine("Sisesta oma perekonnanimi: ");
+            string lastname = Console.ReadLine();
+
+            string fullName = $"{firstName}{lastname}".ToLower(); //ees ja perekonnanimi kokku 
+
+            Console.WriteLine(fullName);
+
+            int aCounter = 0;
+
+            for(int i = 0; i < fullName.Length; i++)
+            {
+                if(fullName[i] == 'a') //char a = 'a';
+                {
+                    aCounter++; 
+                }
+
+            }
+
+            if (aCounter != 1)
+            {
+                 Console.WriteLine($"Sinu täisimes on {aCounter} tähte.");
+            }
+            else
+            {
+                Console.WriteLine($"Sinu täisnimes on {aCounter} täht.");
+            }
+           
+        }
+    }
+}
---
+++ StringsAndArrays/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace StringsAndArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutajal sisestada eesnime
+            //programm kuvab kasutaja eesnime pikkust
+
+            Console.WriteLine("Sisesta eesnimi:");
+            string firstName = Console.ReadLine();
+
+           // int firstNameLenght = firstName.Length; //siia tuleb number eks sõna pikkus
+            Console.WriteLine($"Sinu eesnimes on {firstName.Length} sümbolit."); //see variant sobib kuvab sama tulemust
+
+            Console.WriteLine("Kena päeva!");
+         }
+    }
+}
